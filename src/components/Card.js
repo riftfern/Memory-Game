84c1e4e53a090bc5d5dcf3be13adc31f26dc3912
jack gerow <adriftfern@gmail.com>
2023-03-15 15:48:34 -0400

    added component

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Card(props) {
+  const {
+    card: { src, title },
+    handleGameLogic,
+  } = props;
+
+  return (
+    <button className="card" onClick={handleGameLogic.bind(this, title)}>
+      <figure>
+        <img src={src} alt={title} />
+        <figcaption>{title}</figcaption>
+      </figure>
+    </button>
+  );
+}
