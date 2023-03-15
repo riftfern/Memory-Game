8b01462fefd724bdfca80cd5bcfb0b0b44f5275e
jack gerow <adriftfern@gmail.com>
2023-03-15 15:49:15 -0400

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+export default function Header(props) {
+  const { score, highScore } = props;
+
+  return (
+    <header className="header">
+      <h1 className="main-title">Memory Game</h1>
+      <div className="score-board">
+        <p className="current-score">Score: {score}</p>
+        <p className="high-score">High Score: {highScore}</p>
+      </div>
+    </header>
+  );
+}
