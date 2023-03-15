79ce5758278ace8ef65a92373d22ee854534cf91
jack gerow <adriftfern@gmail.com>
2023-03-15 15:54:29 -0400

    added game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Game from "./components/Game";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
 }
 
 export default App;
