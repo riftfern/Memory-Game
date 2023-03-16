ed755ee1977a6a86dd64e67f584a389f5430c538
jack gerow <adriftfern@gmail.com>
2023-03-16 18:49:42 -0400

    finished project

---
+++ src/images/index.js
@@ -0,0 +1,20 @@
+const IMAGES = {
+  kingOfSwords: require("./king-of-swords.jpeg").default,
+  king0fPentacles: require("./king-of-pantacles.jpeg"),
+  king0fCups: require("./king-of-cups.jpeg"),
+  king0fWands: require("./king-of-wands.jpeg"),
+  queen0fSwords: require("./queen-of-swords.jpeg"),
+  queen0fPentacles: require("./queen-of-pantacles.jpeg"),
+  queen0fWands: require("./king-of-wands.jpeg"),
+  queen0fCups: require("./queen-of-cups.jpeg"),
+  knightOfSwords: require("./knight-of-swords.jpeg"),
+  knightOfPentacles: require("./knight-of-pentacles.jpeg"),
+  knightOfWands: require("./knight-of-wands.jpeg"),
+  knightOfCups: require("./knight-of-cups.jpeg"),
+  pageOfSwords: require("./page-of-swords.jpeg"),
+  pageOfPentacles: require("./page-of-pentacles.jpeg"),
+  pageOfWands: require("./page-of-wands.jpeg"),
+  pageOfCups: require("./page-of-cups.jpeg"),
+};
+
+export default IMAGES;
