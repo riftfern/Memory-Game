4063e1d0279bcfebb2ea8cb84a2bdbc27662647b
jack gerow <adriftfern@gmail.com>
2023-03-17 19:24:29 -0400

    changed title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ export default function Header(props) {
 
   return (
     <header className="header">
-      <h1 className="main-title">Memory Game</h1>
+      <h1 className="main-title">Tarot Memory Game</h1>
       <div className="score-board">
         <p className="current-score">Score: {score}</p>
         <p className="high-score">High Score: {highScore}</p>
